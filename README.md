72f444e40908705b7c8474b5a110bab1b244fe27
Adrian Hilber <adi.hilber@gmail.com>
2021-04-07 20:49:56 +0200

    First working codec server version

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 The AWAH-SIP Codec for using on all sorts of Devices.
 Written by Andy Weiss and Adi Hilber
+
+## Clone Repo
+
+Run `git submodules init` after cloning this repo to load the Library from it's own Repo
+
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <QCoreApplication>
+#include <QSettings>
+#include "awah-sip_library/awahsiplib.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QSettings settings("awah", "AWAH-Sip_Codec");
+    QCoreApplication::setOrganizationName("awah");
+    QCoreApplication::setOrganizationDomain("awah.ch");
+    QCoreApplication::setApplicationName("AWAH-Sip_Codec");
+
+    AWAHSipLib::prepareLib();
+    AWAHSipLib Lib(nullptr);
+
+    return a.exec();
+}
